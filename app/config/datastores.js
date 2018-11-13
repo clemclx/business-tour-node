3f16609d226f256b8801f56b35be26b8e919e7d7
ClementRb <clement.ribourt@gmail.com>
2018-11-13 16:32:28 +0100

    datastore

--- app/config/datastores.js
+++ app/config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://root:root@localhost:3306/Projet',
+    url: 'mysql://root:sails@dbproject:3306/sails_mysql',
 
   },
 
