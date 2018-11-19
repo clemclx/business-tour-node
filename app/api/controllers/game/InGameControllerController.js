23743d860640de2eafd0fa09be7133a2d95b4eb6
ClementRb <clement.ribourt@gmail.com>
2018-11-19 09:29:50 +0100

    ajout de la fonction dé

--- app/api/controllers/game/InGameControllerController.js
+++ app/api/controllers/game/InGameControllerController.js
@@ -16,8 +16,36 @@ module.exports = {
             })
         console.log('Case génerée')    
         }
+    },
+
+    GetAllTiles: async function(req, res){
+        try{
+            let allTiles = await tile.find({
+               
+                select: ['id', 'name', 'price']
+            })
+            console.log(allTiles)
+        } catch (err){
+            sails.log(err)
+        }
+    },
+
+    RollingDice : function(req, res){
+            function randomIntInc(low, high){
+                return Math.floor(Math.random()* (high-low+1) +low)
+              }
+              
+              let numbers = new Array(2)
+              for (let i = 0; i < numbers.length; i++){
+                numbers[i] = randomIntInc(1, 6)
+              }
+              
+              let dice = numbers[0] + numbers[1]
+              
+              console.log(numbers)
+              console.log(dice)
     }
 
 };
 
-module.exports.GenerateTiles();
+module.exports.RollingDice();
--- app/api/models/tile.js
+++ app/api/models/tile.js
@@ -7,11 +7,6 @@ module.exports = {
             type: 'string',
             description: "Tile's name",
         },
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> 0ce771d3f696e9b73a31b5f14deb08e78dee7d52
         price:{
             type: 'number',
             description: "Tile's price",
